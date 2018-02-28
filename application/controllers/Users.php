b2618373f64eb1db0fba548ee7ea286d0f4eb34b
Do Minh Hai <hai.do.intern@ntq-solution.com.vn>
2018-02-28 16:12:48 +0700

    create user model

--- application/controllers/Users.php
+++ application/controllers/Users.php
@@ -8,6 +8,12 @@
 
 class Users extends CI_Controller
 {
+    public function __contructor()
+    {
+        parent::__contructor();
+        $this->load->model("User_model");
+    }
+
     public function index()
     {
         $this->load->database();
@@ -26,5 +32,37 @@ class Users extends CI_Controller
         $users = $query->result();
         $users_jsonencode = json_encode($users);
         echo $users_jsonencode;
+    }
+
+    public function checkUserAccount($email, $password)
+    {
+
+
+        // set result
+        $result['user_id'] = 0;
+        $result['isCorrect'] = false;
+        $result['message'] = "Tài khoản không tồn tại";
+
+        foreach($users as $user){
+            if($email == $user->email){
+                if($password == $user->password){
+                    $result['user_id'] = $user->id;
+                    $result['isCorrect'] = true;
+                    $result['message'] = "Đăng nhập thành công";
+                    break;
+                }else{
+                    $result['message'] = "Mật khẩu đăng nhập sai";
+                }
+            }
+        }
+
+        // json-encode result
+        $result_jsonencode = json_encode($result);
+        return $result_jsonencode;
+    }
+
+    public function registerUserAccount()
+    {
+
     }
 }
